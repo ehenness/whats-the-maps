776da193aa2a02da21557d5a7c66b135ac0f608a
Emma Hennessee <ehenness@trinity.edu>
2026-04-06 17:58:10 -0500

    cleaning up files, css file re-do, some style changes

--- WhatsTheMaps/public/javascript/popup.js
+++ WhatsTheMaps/public/javascript/popup.js
@@ -1,10 +1,10 @@
-const test = document.getElementById("test");
-const popup = document.getElementById("popup");
+const test = document.getElementById('test');
+const popup = document.getElementById('popup');
 
-test.addEventListener("click", event => {
-    if(popup.style.display === "none"){
-        popup.style.display = "block";
-    } else {
-        popup.style.display = "none";
-    }
+test.addEventListener('click', () => {
+  if (popup.style.display === 'none') {
+    popup.style.display = 'block';
+  } else {
+    popup.style.display = 'none';
+  }
 });
