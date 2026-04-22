b4bdcda2ad67a7c7c3960100ce3f6a0e05fd292f
Mitch Onufrow <monufrow@trinity.edu>
2026-04-22 15:56:33 -0500

    new files made

--- WhatsTheMaps/gameData.js
+++ WhatsTheMaps/gameData.js
@@ -1,4 +1,4 @@
-const db = require('./db');
+const db = require('./repositories/db');
 
 const QUESTION_TIME_LIMIT_MS = 15_000;
 const MAX_BASE_SCORE = 1_000;
--- WhatsTheMaps/routes/index.routes.js
+++ WhatsTheMaps/routes/index.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const db = require('../repositories/db');
 const { listPresetProfileImages } = require('../profileConfig');
 const { getStoredProfile } = require('../profileStore');
 const {
